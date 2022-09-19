38b9e8364102531519809dc8780e8ab623b82722
aswajith <aswajith.jithu888@gmail.com>
2022-09-19 16:48:17 +0530

    dynamic click

--- productlist.js
+++ productlist.js
@@ -1,4 +1,31 @@
 var count = 0;
+var MobilePrice;
+(function (MobilePrice) {
+    MobilePrice[MobilePrice["Black"] = 250] = "Black";
+    MobilePrice[MobilePrice["Gold"] = 280] = "Gold";
+    MobilePrice[MobilePrice["White"] = 300] = "White";
+})(MobilePrice || (MobilePrice = {}));
+;
+var MobileCount;
+(function (MobileCount) {
+    MobileCount[MobileCount["Black"] = 10] = "Black";
+    MobileCount[MobileCount["Gold"] = 0] = "Gold";
+    MobileCount[MobileCount["White"] = 4] = "White";
+})(MobileCount || (MobileCount = {}));
+;
+var MobileDiscount;
+(function (MobileDiscount) {
+    MobileDiscount[MobileDiscount["Black"] = 5] = "Black";
+    MobileDiscount[MobileDiscount["Gold"] = 10] = "Gold";
+    MobileDiscount[MobileDiscount["White"] = 5] = "White";
+})(MobileDiscount || (MobileDiscount = {}));
+;
+function getPrice(color) {
+    document.getElementById("pPrice").innerHTML = MobilePrice[color];
+    document.getElementById("pAvailable").innerHTML = isAvailable(MobileCount[color]);
+    document.getElementById("pDiscount").innerHTML = MobileDiscount[color];
+    return MobilePrice[color];
+}
 function isAvailable(n) {
     var available = 'available';
     if (n <= 0) {
@@ -7,5 +34,6 @@ function isAvailable(n) {
     return available;
 }
 document.getElementById("pName").innerHTML = 'Samsug Galaxy s22';
-document.getElementById("pPrice").innerHTML = '999';
-document.getElementById("pAvailable").innerHTML = isAvailable(count);
+document.getElementById("Black").innerHTML = getPrice('Black');
+document.getElementById("Gold").innerHTML = getPrice('Gold');
+document.getElementById("White").innerHTML = getPrice('White');
--- productlist.ts
+++ productlist.ts
@@ -1,4 +1,19 @@
 let count:number = 0;
+
+enum MobilePrice {Black= 250, Gold= 280, White= 300};
+enum MobileCount {Black= 10, Gold= 0, White= 4};
+enum MobileDiscount {Black= 5, Gold= 10, White= 5};
+
+
+function getPrice(color:string)
+{
+document.getElementById("pPrice").innerHTML= MobilePrice[color];
+document.getElementById("pAvailable").innerHTML=isAvailable(MobileCount[color]);
+document.getElementById("pDiscount").innerHTML=MobileDiscount[color];
+
+return MobilePrice[color];
+}
+
 function isAvailable(n:number):string
 {
     let available:string= 'available';
@@ -8,6 +23,10 @@ function isAvailable(n:number):string
     }
     return available;
 }
+
 document.getElementById("pName").innerHTML='Samsug Galaxy s22';
-document.getElementById("pPrice").innerHTML= '999';
-document.getElementById("pAvailable").innerHTML=isAvailable(count);
+
+document.getElementById("Black").innerHTML=getPrice('Black');
+document.getElementById("Gold").innerHTML=getPrice('Gold');
+document.getElementById("White").innerHTML=getPrice('White');
+
